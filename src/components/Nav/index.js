e70c291bd6a358c253df41817347a8b46482e4f7
Kareem Rizk <kareem.magdy5@gmail.com>
2022-04-11 15:17:01 -0400

    changed name

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -13,7 +13,7 @@ function Nav(props) {
     <header className="flex-row px-1">
       <h2>
         <a data-testid="link" href="/">
-          <span role="img" aria-label="camera"> 📸</span> Oh Snap!
+          <span role="img" aria-label="camera"> </span> Kareem Rizk
         </a>
       </h2>
       <nav>
