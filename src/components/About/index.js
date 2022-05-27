e6de9c84a0f674e5cd56a808adcd62cb8ea5f324
Kareem Rizk <kareem.magdy5@gmail.com>
2022-05-26 21:06:51 -0400

    updated bio

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -9,7 +9,7 @@ function About() {
             <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
             <div className="my-2">
             <p>
-            Full stack developer with a passion for programming and information technology; currently open to work. With a web development certification from Washington University and nearly 10 years experience as a journalist and translator. Open to both front-end and back-end positions. What's more important is finding the right team and building something great.
+            Full stack developer with a passion for programming and information technology; currently open to work. With a web development certification from George Washington University and nearly 10 years experience as a journalist and translator. Open to both front-end and back-end positions. What's more important is finding the right team and building something great.
             </p>
             </div>
         </section>
